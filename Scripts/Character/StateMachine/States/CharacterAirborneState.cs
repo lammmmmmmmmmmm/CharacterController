f47d7ad8e80cf824731ec95ef80300c562d14725
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2026-02-26 18:15:37 +0700

    fix: transitioning to grounded state while jumping up

--- Scripts/Character/StateMachine/States/CharacterAirborneState.cs
+++ Scripts/Character/StateMachine/States/CharacterAirborneState.cs
@@ -4,8 +4,11 @@ namespace PhysicsCharacterController.CharacterStateMachine.States
 {
     public sealed class CharacterAirborneState : State
     {
+        private const float MINIMUM_AIRBORNE_SECONDS = 0.1f;
+
         private readonly CharacterStateContext _context;
         private bool _isPlayingJumpClip;
+        private float _minimumAirborneTimer;
 
         public CharacterAirborneState(StateMachine machine, State parent, CharacterStateContext context)
             : base(machine, parent)
@@ -20,7 +23,7 @@ namespace PhysicsCharacterController.CharacterStateMachine.States
                 return null;
             }
 
-            if (_context.IsGrounded && _context.CharacterGravity.VerticalVelocity <= 0f)
+            if (_context.IsGrounded && _minimumAirborneTimer <= 0f)
             {
                 return ((CharacterRootState)Parent).Grounded;
             }
@@ -30,12 +33,19 @@ namespace PhysicsCharacterController.CharacterStateMachine.States
 
         protected override void OnEnter()
         {
+            _minimumAirborneTimer = MINIMUM_AIRBORNE_SECONDS;
+
             bool isRising = _context.CharacterGravity.VerticalVelocity >= 0f;
             PlayAirborneClip(isRising);
         }
 
         protected override void OnFixedUpdate(float fixedDeltaTime)
         {
+            if (_minimumAirborneTimer > 0f)
+            {
+                _minimumAirborneTimer -= fixedDeltaTime;
+            }
+
             _context.Animator.UpdateTransitionMixerParameter(_context.CharacterMove.CurrentSpeed);
 
             bool isRising = _context.CharacterGravity.VerticalVelocity >= 0f;
